e312ffde524bf50c2a23ab55fa6bd9311d85ab9b
artermi <ericcheng230@gmail.com>
2023-04-17 12:31:11 +0100

    ,mgorog

---
+++ main_all_gd.cpp
@@ -0,0 +1,56 @@
+#include "QuasiPD_grid.h"
+#include <unistd.h>
+using namespace std;
+
+int do_once(double a,double b,double l,double m,double d,double r){
+
+	char file_n[100];
+	sprintf(file_n,"A_%04d_b_%04d_l_%04d_m_%04d_d_%04d_r_%04d.dat", 
+		(int)((a + 0.000001) * 100), (int)((b + 0.000001) * 100),
+		(int)((l + 0.000001) * 100), (int)((m + 0.000001) * 100),
+		(int)((d + 0.000001) * 100), (int)((r + 0.000001) * 100));
+
+	FILE *file;
+		file = fopen(file_n, "r");
+
+	if (file) {
+		fclose(file);
+		printf("file:'%s' exists\n",file_n);
+		usleep(100000);
+		return 0;
+	}
+
+	file = fopen(file_n, "w");
+	fclose(file);
+
+	QuasiPD_grid gameOBJ(a,r,b,l,d,m);
+	gameOBJ.game(true);
+
+	return 0;
+}
+
+
+int main(int argc, char** argv){
+	srand(time(NULL));
+
+	double gap = 0.2;
+
+
+	for(double b = 1.0; b < 2.01; b += gap ){
+		for (double a = 0.0; a < 1.01; a += gap){
+			for(double l = 0.0; l < 1.01; l += gap){
+				for(double m = 0.0; m < 1.01; m += gap){
+					for (double d = 0.0; d < 1.01; d += gap){
+						for (double r = 0.0; r < 1.01; r += gap){
+							do_once(a,b,l,m,d,r);
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+
+	return 0;
+}
